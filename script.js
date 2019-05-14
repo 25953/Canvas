be4091e50cedaa961b2df22f5435812b2411c0e1
Kevin <42716770+25953@users.noreply.github.com>
2019-05-14 11:51:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+let canvas = document.getElementById("myCanvas");
+let context = canvas.getContext("2d");
+//oor links
+context.beginPath();
+context.arc(80,80,40,0,2*Math.PI);
+context.fillStyle = "#934a00"
+context.closePath();
+context.stroke();
+context.fill();
+//oor2 links
+context.beginPath();
+context.arc(90,90,20,0,2*Math.PI);
+context.fillStyle = "#141414"
+context.closePath();
+context.stroke();
+context.fill();
+//oor rechts
+context.beginPath();
+context.arc(320,80,40,0,2*Math.PI);
+context.fillStyle = "#934a00"
+context.closePath();
+context.stroke();
+context.fill();
+//oor2 rechts
+context.beginPath();
+context.arc(310,90,20,0,2*Math.PI);
+context.fillStyle = "#141414"
+context.closePath();
+context.stroke();
+context.fill();
+//main cirkel
+context.beginPath();
+context.arc(200,200,150,0,2*Math.PI);
+context.fillStyle = "#934a00"
+context.closePath();
+context.stroke();
+context.fill();
+//oog links
+context.beginPath();
+context.arc(130,130,40,0,2*Math.PI);
+context.fillStyle = "#fff"
+context.closePath();
+context.stroke();
+context.fill();
+//iris links
+context.beginPath();
+context.arc(130,130,23,0,2*Math.PI);
+context.fillStyle = "#6edb00"
+context.closePath();
+context.stroke();
+context.fill();
+//pupil links
+context.beginPath();
+context.arc(130,130,20,0,2*Math.PI);
+context.fillStyle = "#141414"
+context.closePath();
+context.stroke();
+context.fill();
+//oog rechts
+context.beginPath();
+context.arc(270,130,40,0,2*Math.PI);
+context.fillStyle = "#fff"
+context.closePath();
+context.stroke();
+context.fill();
+//iris rechts
+context.beginPath();
+context.arc(270,130,23,0,2*Math.PI);
+context.fillStyle = "#6edb00"
+context.closePath();
+context.stroke();
+context.fill();
+//pupil rechts
+context.beginPath();
+context.arc(270,130,20,0,2*Math.PI);
+context.fillStyle = "#141414"
+context.closePath();
+context.stroke();
+context.fill();
+//mond
+context.beginPath();
+context.arc(200,230,90,0,1*Math.PI);
+context.fillStyle = "#ff2424"
+context.closePath();
+context.stroke();
+context.fill();
+//neus1
+context.beginPath();
+context.arc(200,190,25,0,1*Math.PI);
+context.fillStyle = "#141414"
+context.closePath();
+context.stroke();
+context.fill();
